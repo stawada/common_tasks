ab091a26a0809ff33d1f3e718d14b3e298775c34
yuusyamada-2002 <yuusyamada@fab.pasona.tech>
2023-07-31 15:53:41 +0900

    コピペしたよ

--- backend/DB.go
+++ backend/DB.go
@@ -1,10 +1,48 @@
 package main
 
-// import (
-// 	"fmt"
-// 	"io"
-// 	"os"
-// 	"time"
+import (
+	"os"
 
-// 	"gorm.io/gorm"
-// )
+	"github.com/goark/errs"
+	"github.com/goark/gocli/exitcode"
+	"github.com/jackc/pgx/v4"
+	"github.com/jackc/pgx/v4/log/zerologadapter"
+	"github.com/jackc/pgx/v4/stdlib"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+func Run() exitcode.ExitCode {
+	// get logger
+	zlogger := CreateLogger()
+
+	// create gorm.DB instance for PostgreSQL service
+	cfg, err := pgx.ParseConfig(os.Getenv("ELEPHANTSQL_URL"))
+	if err != nil {
+		zlogger.Error().Interface("error", errs.Wrap(err)).Send()
+		return exitcode.Abnormal
+	}
+	cfg.Logger = zerologadapter.NewLogger(zlogger)
+	cfg.LogLevel = pgx.LogLevelDebug
+	db, err := gorm.Open(postgres.New(postgres.Config{
+		Conn: stdlib.OpenDB(*cfg),
+	}), &gorm.Config{
+		Logger: logger.Discard,
+	})
+	defer func() {
+		if sqlDb, err := db.DB(); err == nil {
+			sqlDb.Close()
+		}
+	}()
+
+	// query
+	var results []map[string]interface{}
+	tx := db.Table("tablename").Find(&results) // "tablename" is not exist
+	if tx.Error != nil {
+		zlogger.Error().Interface("error", errs.Wrap(tx.Error)).Send()
+		return exitcode.Abnormal
+	}
+
+	return exitcode.Normal
+}
