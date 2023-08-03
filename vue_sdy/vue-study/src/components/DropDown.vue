4ce6faccdf7269cb49cd3a9d11abc630163f369e
yu-jima <yuiijima@fab.pasona.tech>
2023-08-03 09:51:10 +0900

    test

---
+++ vue_sdy/vue-study/src/components/DropDown.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="dropdown" style="width:100%; display:flex; justify-content: center; align-items: center;">
+        <select>
+            <option value="math">数学</option>
+            <option value="japanese">国語</option>
+            <option value="english">英語</option>
+        </select>
+    </div>
+</template>
