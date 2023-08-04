f67ab20a952a93faba218d0c45b40b722e80d3bf
yu-jima <yuiijima@fab.pasona.tech>
2023-08-04 10:51:46 +0900

    欠席確認画面

---
+++ vue_sdy/vue-study/src/AbsentCheck.vue
@@ -0,0 +1,44 @@
+<template>
+    <header>
+      <div class="container">
+        <img src="@/assets/check.png" alt="check" class="check-img"/>
+      </div>
+    </header>
+    <body>
+      <p class="massage">欠席登録できました。</p>
+    </body>
+  </template>
+  
+  <style>
+  html {
+    background-color: gray;
+  }
+  
+  header {
+    margin-top: 100px;
+  }
+  
+  .container {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: auto;
+  }
+  
+  .check-img {
+    width: 100px;
+    height: 100px;
+  }
+  
+  .massage{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: auto;
+    margin-top: 50px;
+    color: white;
+  }
+  
+  </style>
