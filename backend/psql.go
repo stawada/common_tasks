0c91c02ee4c8849bc4f7802319fb506dc5abffea
yuusyamada-2002 <yuusyamada@fab.pasona.tech>
2023-08-04 15:40:31 +0900

    出席可能講義の出力

--- backend/psql.go
+++ backend/psql.go
@@ -2,10 +2,9 @@
 package main
 
 import (
-	//"encoding/json" //json関連
 	"database/sql" //DB操作
 	"fmt"
-	//"io/ioutil" //ファイル読み込み
+	"log"
 
 	_"github.com/lib/pq"
 )
@@ -18,6 +17,7 @@ type App struct {
 	Attendance_flag int
 	Lecture_catalog_id string
 	Lecture_id string
+	Lecture_name string
 	Lecture_history_id string
 	lecture_data_and_time int
 }
@@ -35,16 +35,7 @@ func checkError(err error){ //エラーの確認
 	}
 }
 
-/*func readJson(){ //jsonの読み込み
-	s, _ := ioutil.ReadFile("sample_1.json")
-	var jsondata map[string]interface{}
-	json.Unmarshal([]byte(s), &jsondata)
-	fmt.Println(jsondata)
-
-	return
-}*/
-
-func main(){ //DBへの書き込み
+func attendance_update(){ //出欠席
 	var connectionString string = fmt.Sprintf("host=%s user=%s dbname=%s sslmode=disable", HOST, USER, DATABASE)  //sslmode=disableにてSSLモードを無効
 
 	db, err := sql.Open("postgres", connectionString)
@@ -52,11 +43,46 @@ func main(){ //DBへの書き込み
 
 	err = db.Ping()
 	checkError(err)
-	    //fmt.Println("Finished dropping table (if existed)")
 
-	//readJson() //jsonファイルの読み込み
-    sql_statement := "UPDATE attendance_information SET attendance_flag = %d WHERE student_id='%s' AND lecture_history_id='%s';" //update文の実行(フラグ管理)
+	sql_statement := `UPDATE attendance_information 
+					  SET attendance_flag = %d
+					  WHERE student_id='%s' AND lecture_history_id='%s';` //update文の実行(フラグ管理)
 	_, err = db.Exec(sql_statement)
 	checkError(err)
+}
 
+func main(){//出席可能講義
+	var connectionString string = fmt.Sprintf("host=%s user=%s dbname=%s sslmode=disable", HOST, USER, DATABASE)  //sslmode=disableにてSSLモードを無効
+
+	db, err := sql.Open("postgres", connectionString)
+	checkError(err)
+
+	err = db.Ping()
+	checkError(err)
+
+	sql_statement := `SELECT lecture_catalog.lecture_name
+					  FROM((attendance_information
+						INNER JOIN lecture_history ON attendance_information.lecture_history_id = lecture_history.lecture_history_id)
+						INNER JOIN lecture_catalog ON lecture_history.lecture_catalog_id = lecture_history.lecture_catalog_id)
+				 	 WHERE student_id = '1000000000' and lecture_history.lecture_data_and_time-5 <= 1000 and lecture_history.lecture_data_and_time+5 >= 1000;` //SELECT文の実行(出席可能講義の抽出)
+	rows, err := db.Query(sql_statement)
+	checkError(err)
+	if err != nil{
+		log.Println(err)
+	}
+	defer rows.Close()
+
+	var u App
+
+	for rows.Next(){
+		err := rows.Scan(&u.Lecture_name)
+		if err != nil{
+			log.Fatal(err)
+		}
+		fmt.Printf("%s\n", u.Lecture_name)
+	}
+	err = rows.Err()
+	if err != nil{
+		log.Fatal(err)
+	}
 }
