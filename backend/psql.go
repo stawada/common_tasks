1a254f59404574a408c297594242d642440baf70
yuusyamada-2002 <yuusyamada@fab.pasona.tech>
2023-08-04 10:06:03 +0900

    本番を想定したテストを想定したDB書きこみ用ファイル

--- backend/psql.go
+++ backend/psql.go
@@ -2,10 +2,10 @@
 package main
 
 import (
-	"encoding/json"
-	"database/sql"
+	//"encoding/json" //json関連
+	"database/sql" //DB操作
 	"fmt"
-	"io/ioutil"
+	//"io/ioutil" //ファイル読み込み
 
 	_"github.com/lib/pq"
 )
@@ -22,29 +22,29 @@ type App struct {
 	lecture_data_and_time int
 }
 
-const (
+const ( //接続するDBの情報
 	HOST = "localhost"    //ホスト名
 	DATABASE = "demo_db"    //データベースの名前
 	USER = "yuusyamada"    //ユーザーネーム
 	PASSWORD = "Pasona123"
 )
 
-func checkError(err error){
+func checkError(err error){ //エラーの確認
 	if err != nil {
 		panic(err)
 	}
 }
 
-func readJson(){
+/*func readJson(){ //jsonの読み込み
 	s, _ := ioutil.ReadFile("sample_1.json")
 	var jsondata map[string]interface{}
 	json.Unmarshal([]byte(s), &jsondata)
 	fmt.Println(jsondata)
 
 	return
-}
+}*/
 
-func main(){
+func main(){ //DBへの書き込み
 	var connectionString string = fmt.Sprintf("host=%s user=%s dbname=%s sslmode=disable", HOST, USER, DATABASE)  //sslmode=disableにてSSLモードを無効
 
 	db, err := sql.Open("postgres", connectionString)
@@ -52,10 +52,10 @@ func main(){
 
 	err = db.Ping()
 	checkError(err)
-	    fmt.Println("Finished dropping table (if existed)")
+	    //fmt.Println("Finished dropping table (if existed)")
 
-	readJson() //jsonファイルの読み込み
-    sql_statement := "UPDATE attendance_information SET attendance_flag = %d WHERE student_id='%s' AND lecture_history_id='%s';", Attendance_flag, Student_id, Lecture_history_id //WHEREでどこのフラグが何になるかを受け取れるようにする
+	//readJson() //jsonファイルの読み込み
+    sql_statement := "UPDATE attendance_information SET attendance_flag = %d WHERE student_id='%s' AND lecture_history_id='%s';", //update文の実行(フラグ管理)
 	_, err = db.Exec(sql_statement)
 	checkError(err)
 
