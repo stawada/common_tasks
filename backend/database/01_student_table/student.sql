0b5078071ee8e9fad373ea833d9b7c53c83cc01c
stawada <stawada@fab.pasona.tech>
2023-08-09 17:18:54 +0900

    4つのテーブルを作成するプログラムとサンプルデータ

---
+++ backend/database/01_student_table/student.sql
@@ -0,0 +1,13 @@
+DROP TABLE student;
+CREATE TABLE student (student_id varchar(10) PRIMARY KEY, student_name varchar(50), hashed_password char(64));
+
+INSERT INTO student values ('S000000000', 'm', 'e78ef78eb2d30233b6931b98636e46cc1cf97d2b2ea410646ae5d6bc13023639');
+INSERT INTO student values ('B000000001', 'sbmhrf', '5d27ce3ad19f394dd3d4b19b088aeeac13dba91bf4b187a7c0fae84d8825882f');
+INSERT INTO student values ('F000000002', 'mvmhcdxvew', '2c7221c77e22a4ccfa9f0f5de5678867033efa5bd6dc4084455cc3019ce8f502');
+INSERT INTO student values ('O000000003', 'lubifkyadraldoyyufkfbcopunvw', '1c830a59ae0c0b151f75348aa5f4aee9648df5b5fe80ad052da59b8d9e1b787a');
+INSERT INTO student values ('W000000004', 'dtqropwdebnxinpgfr', '5c8fa544b777336190a5374f9b1ef658f9071f0be9ea764ae161fb813bad99d6');
+INSERT INTO student values ('J000000005', 'zc', '205f955829907df94a26d1a1482fa304f3dac7b3a337e840357bab022e5e0adf');
+INSERT INTO student values ('R000000006', 'uukxaajdbkeuqbudzfu', 'bf4b1dd94f63e35e363075331def7eefe655af0e23ff70517b335b9f308451b1');
+INSERT INTO student values ('O000000007', 'eciblgjs', 'a395732918e1a7f98dc84b95aa0cf445e7024bd2b8da4d68ae9a97d402662c96');
+INSERT INTO student values ('F000000008', 'omprmetxtmlfppp', 'dd334a64e7487393ad1f746eaac7e234df7deec6ef478011d247eae61563e706');
+INSERT INTO student values ('P000000009', 'yhhe', 'ae12adafa482f2a3c742537fff3c3f0336254680c60a0d30719cfc95d8ac468b');
