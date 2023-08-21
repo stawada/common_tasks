d096dea9396afde929309f899ea2e75098582d58
yu-jima <yuiijima@fab.pasona.tech>
2023-08-21 09:49:09 +0900

    暫定版

--- vue-test-project/src/components/DropDown.vue
+++ vue-test-project/src/components/DropDown.vue
@@ -62,7 +62,6 @@ export default {
         },
 
         onEmit(){
-            console.log(this.selectSubject,this.lectureDateAndTime,this.dic)
             this.$emit("onChange",this.selectSubject,this.lectureDateAndTime,this.dic)
         },
 
@@ -72,21 +71,15 @@ export default {
             this.key = this.selectSubject;
             this.subTimeKey = Object.keys(this.subjectTime)
             this.subTimeValues = Object.values(this.subjectTime)
-            console.log(this.subTimeKey,this.subTimeValues)
 
             for(let i=0; i<this.subjectName.length; i++){
                 if(this.key == this.subTimeKey[i]){
                     this.date.push(this.subTimeValues[i])
-                    console.log(this.date)
-                    //this.dateTime.push(new Date(this.date*1000).toLocaleString());
-                    // console.log(this.dateTime)
                 }
             }
             this.date[0].sort();
-            console.log(this.date);
             for(let i=0; i<this.date[0].length; i++){
                 var newDate = this.date[0][i]
-                console.log(newDate)
                 this.dateTime.push(new Date(newDate*1000).toLocaleString())
             }
 
@@ -94,14 +87,11 @@ export default {
             for(let i=0;i<this.subTimeKey.length;i++){
                 this.dic[this.subTimeKey[i]] = {};
             }
-            console.log(this.dic)
-            console.log(this.responseJSON)
             for(let i=0;i<this.responseJSON.data.length;i++){
                 var subjectname = this.responseJSON.data[i].subject_name;
                 var subjectid = this.responseJSON.data[i].subject_id;
                 var subjecttime = this.responseJSON.data[i].subject_time;
                 this.dic[subjectname][subjecttime] = subjectid;
-            console.log(this.dic)
             }
         }
     }
@@ -114,8 +104,6 @@ export default {
     display: flex;
     flex-direction: column;
     justify-content: center;
-    /* align-items: center; */
-
 }
 
 .headline {
--- vue-test-project/src/pages/Absent.vue
+++ vue-test-project/src/pages/Absent.vue
@@ -12,8 +12,6 @@ export default {
     Header,
     Footer
   },
-  setup(){
-      },
 
     data (){
 
@@ -32,88 +30,63 @@ export default {
         }
     },
     methods: {
-        //出席ボタンを押した時のイベント
-        // onclick(){
-        //     this.button_clicked = true;
-        //     alert("버튼 클릭");
-        //     setTimeout(()=>{
-        //         this.$router.push('/attendance_check')
-        //     },1000);
-        //     axios.post(this.BASE_URL + 'attend',
-        //     {
-        //         attend_flag : 1,
-        //         attend_day : Date.now(),
-        //         student_id : "111111",
-        //         subject_id : this.subject_id
-        //     })
-        //     .then(
-        //         response=>console.log(response)
-        //         this.$router.push('/attendance_check')
-        //     ).catch(error => console.log(error))
-        // }
-
-        getVal(selectSubject, lectureDateAndTime, dic){
+        getVal(selectSubject, lectureDateAndTime, dic){ //DropDownからのデータ受けとり
           this.subject = selectSubject,
           this.dateTime = lectureDateAndTime,
           this.dic = dic,
-          this.dateUTC = (new Date(this.dateTime).getTime())/1000,
-          console.log(this.subject,this.dateUTC,this.dic);
-          //this.subject_id = this.responseJSON.find((this.subject,this.dateUTC))
+          this.dateUTC = (new Date(this.dateTime).getTime())/1000
         },
 
-        onClick(){
+        onClick(){ //欠席ボタン押下時の処理
           this.ans = this.dic[this.subject][this.dateUTC]
-          console.log('検索結果:'+this.ans)
-          // this.ans = this.ans[this.dateUTC]
-          // console.log(this.ans)
             this.button_clicked = true;
             const nowTime = Math.floor((new Date()).getTime() / 1000)
+
             axios.post(
                 this.BASE_URL + 'attend',
                 {
                     attend_flag : -1,
                     now_time : nowTime,      //!!!!!!!!!!nowTime
-                    student_id : "G000000000",  //!!!!!!!!!!user_id
+                    student_id : "G000000000",  //!!!!!!!!!!user_id ←保持しているuser_idの表記方法が決まり次第修正
                     subject_id : this.ans
                 }
             ).then( response => {
                 console.log(response)
                 const object = response.data
                 if(object.http_status < 400){
-                    if(object.check_flag>0){
-                        setTimeout(() => {this.$router.push('/absent_check')}, 500);
-                    }else if(object.http_status == 400){
-                        alert("BadRequest")
-                        this.$router.go(0)
-                    }else if(object.http_status == 401){
-                        alert("Unauthorized")
-                        this.$router.go(0)
-                    }else if(object.http_status == 404){
-                        alert("Not found")
-                        this.$router.go(0)
-                    }else if(object.http_status == 412){
-                        alert("Precondition Failed")
-                        this.$router.go(0)
-                    }else if(object.http_status == 429){
-                        alert("To Many Requests")
-                        this.$router.go(0)
-                    }else if(object.http_status == 503){
-                        alert("Service Unavailable")
-                        this.$router.go(0)
-                    }else if(object.http_status == 504){
-                        alert("Gateway Timeout")
-                        this.$router.go(0)
-                    }else{
-                        alert("Unknown Error")
-                        this.$router.go(0)
-                    }
+                  if(object.check_flag>-1){
+                      this.button_clicked = true
+                      setTimeout(() => {this.$router.push('/absent_check')}, 500);
                 }else{
-                    alert("ネットワークエラーが発生しました。")
-                    this.$router.go(0)
+                  if(object.http_status == 400){
+                      alert("BadRequest")
+                      this.$router.go(0)
+                  }else if(object.http_status == 401){
+                      alert("Unauthorized")
+                      this.$router.go(0)
+                  }else if(object.http_status == 404){
+                      alert("Not found")
+                      this.$router.go(0)
+                  }else if(object.http_status == 412){
+                      alert("Precondition Failed")
+                      this.$router.go(0)
+                  }else if(object.http_status == 429){
+                      alert("To Many Requests")
+                      this.$router.go(0)
+                  }else if(object.http_status == 503){
+                      alert("Service Unavailable")
+                      this.$router.go(0)
+                  }else if(object.http_status == 504){
+                      alert("Gateway Timeout")
+                      this.$router.go(0)
+                  }else{
+                      alert("Unknown Error")
+                      this.$router.go(0)
+                  }
                 }
+              }
             }).catch(
                 error=>{
-                    this.can_attend = false
                     this.button_clicked = true
                     console.log(error.response)
                     const status = error.response.status
@@ -124,46 +97,63 @@ export default {
     },
 
     created(){
-      axios.post(this.BASE_URL + 'reload' //?????
+      axios.post(this.BASE_URL + 'reload'
             ,{
-                student_id: 'G000000000',  //!!!!!저장소에서 학생아이디 꺼내기
-                //now_time : nowTime
+                student_id: 'G000000000',
+                //now_time : nowTime //AttendのnowTimeを引っ張ってくる
             }
             ).then(
               response => {
-                this.responseJSON = response
-                console.log(this.responseJSON)
+                const object = response.data
+                if(object.http_status < 400){
+                  this.responseJSON = response
                 var set = new Set();
                 for(let i=0; i<response.data.length; i++){
                   set.add(response.data[i].subject_name)
                 }
                 this.subjectName = Array.from(set);
-                //console.log(this.subjectName);
 
                 for(let i=0; i<response.data.length; i++){
-                  // console.log(this.subjectTime[i][response.data[i].subject_name] , response.data[i].subject_time)
-                  // this.subjectTime[response.data[i].subject_name] = response.data[i].subject_time
                   this.subjectTime[this.subjectName[i]] = []
                 }
-                console.log(this.subjectTime)
                 for(let i=0; i<response.data.length; i++){
                   this.subjectTime[response.data[i].subject_name].push(response.data[i].subject_time)
                 }
-                console.log(this.subjectTime)
-            },
-            //response => this.subjectName = response.data,
-            //         if(response.status>0){
-            //             this.can_attend = true;
-            //             this.can_attend_subject = "math"
-            //             this.subject_id = '111111'
-            //         }else{
-            //             this.can_attend = false
-            //         }
-            ).catch(console => console.log('error'))
-
+                }else{
+                  if(object.http_status == 400){
+                      alert("BadRequest")
+                      this.$router.go(0)
+                  }else if(object.http_status == 401){
+                      alert("Unauthorized")
+                      this.$router.go(0)
+                  }else if(object.http_status == 404){
+                      alert("Not found")
+                      this.$router.go(0)
+                  }else if(object.http_status == 412){
+                      alert("Precondition Failed")
+                      this.$router.go(0)
+                  }else if(object.http_status == 429){
+                      alert("To Many Requests")
+                      this.$router.go(0)
+                  }else if(object.http_status == 503){
+                      alert("Service Unavailable")
+                      this.$router.go(0)
+                  }else if(object.http_status == 504){
+                      alert("Gateway Timeout")
+                      this.$router.go(0)
+                  }else{
+                      alert("Unknown Error")
+                      this.$router.go(0)
+                  }
+                }
+              }
+            ).catch(error=>{
+                    this.button_clicked = true
+                    console.log(error.response)
+                    const status = error.response.status
+                    const message = error.response.data.message
+                    alert(status + "ERROR : " + message)})
     }
-
-    // console.log(this.selectSubject,this.lectureDateAndTime)
 };
 </script>
 
@@ -181,9 +171,6 @@ export default {
       </div>
       <div>
         <AbsButton @handleAbsent="onClick"/>
-      </div>
-      <div>
-        <!-- <p>{{ this.subjectTime }}</p> -->
       </div>
     </body>
     <Footer />
