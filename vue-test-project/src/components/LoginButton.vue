71e8548db921096d5ac991b16aeae6d0c228188c
ranran38 <109572648+ranran38@users.noreply.github.com>
2023-08-25 16:50:25 +0900

    modify button css

--- vue-test-project/src/components/LoginButton.vue
+++ vue-test-project/src/components/LoginButton.vue
@@ -8,22 +8,29 @@
     export default{
         name:'LoginButton',
         methods: {
-            
-            // handleAbsent() {
-            //     console.log("ボタンを押しました");
-            // }
         }
     }
 </script>
 
 <style>
 .LoginButton button{
-    border: 0;
-    height: 47px;
-    width: 160px;
+    all: unset;
+    height: 55px;
+    width: 170px;
+
+    background-color: #696969;
+	border: 3px solid white;
+	border-radius: 100px;
+    box-shadow: 0 2px 7px rgba(0, 0, 0, .3);
 
     color: white;
-    font-size: 32px;
-    background-color: #787878;
+    font-size: 20px;
+    font-weight: 600;
+}
+.LoginButton button:active{
+	color: #fff;
+	background: #5a5a5a;
+	border-bottom: 3px solid #fff;
+	transform: translateY(5px);
 }
 </style>
